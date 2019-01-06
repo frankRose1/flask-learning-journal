559ea8d1a97e423a4ceba85c0346e834261f5883
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-01-06 00:26:03 -0500

    Set up routes for authenticating users

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import (Flask, render_template, g, flash, redirect, url_for)
+from flask_login import (login_user, logout_user, login_required,
+						 current_user, LoginManager)
+from flask_bcrypt import check_password_hash
 
 import models
 import forms
@@ -6,17 +9,31 @@ from config import config
 
 app = Flask(__name__)
 app.secret_key = config['secret']
+login_manager = LoginManager()
+login_manager.init_app(app)
+# which view handles logging users in
+login_manager.login_view = 'login'
 
 DEBUG = True
 HOST = '0.0.0.0'
 PORT = 8000
 
 
+@login_manager.user_loader
+def user_loader(userId):
+	"""For when the login manager needs to look up a user"""
+	try:
+		return models.User.get().where(models.User.id == userId)
+	except models.DoesNotExist:
+		return None
+
+
 @app.before_request
 def before_request():
-	"""Connect to the DB before each request"""
+	"""Connect to the DB and set the current user before each request"""
 	g.db = models.DATABASE
 	g.db.connect()
+	g.user = current_user
 
 
 @app.after_request
@@ -29,6 +46,49 @@ def after_request(request):
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = forms.LoginForm()
+	if form.validate_on_submit():
+		try:
+			user = models.User.get().where(models.User.email == form.email.data)
+		except models.DoesNotExist:
+			flash('Incorrect email or password', 'error')
+		else:
+			if check_password_hash(user.password, form.password.data):
+				# set up the session with flask login
+				login_user(user)
+				flash('You are now signed in!', 'success')
+				return redirect(url_for('index'))
+			else:
+				flash('Incorrect email or password', 'error')
+	return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = forms.RegisterForm()
+	if form.validate_on_submit():
+		models.User.create_user(
+			username=form.username.data,
+			email=form.email.data,
+			password=form.password.data
+		)
+		flash('You successfully signed up! Log in to get started.', 'success')
+		# prompt the user to log in
+		return redirect(url_for('login'))
+	return render_template('register.html', form=form)
+
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+	# delete the session cookie
+	logout_user()
+	flash('You are now logged out', 'success')
+	return redirect(url_for('index'))
 
 
 @app.route('/entry/add', methods=['GET', 'POST'])
